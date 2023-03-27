e812e4204a3a59c0b5029cc10598809880b4b86a
Ghulam Subbhani <gsmalik030@gmail.com>
2023-03-27 10:42:14 +0200

    Created Table named animals

--- schema.sql
+++ schema.sql
@@ -1,1 +1,10 @@
 CREATE DATABASE vet_clinic
+
+CREATE TABLE animals(
+	id integer PRIMARY KEY,
+	name INTEGER NOT NULL,
+	date_of_birth DATE NOT NULL,
+	escape_attempts INTEGER NOT NULL,
+	neutered BOOLEAN NOT NULL,
+	weight_kg DECIMAL
+)
