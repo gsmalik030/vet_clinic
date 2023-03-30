a2363687a0b6a8e3892609ff08605166ff7da1af
Ghulam Subbhani <gsmalik030@gmail.com>
2023-03-30 11:05:13 +0200

    Added tables and modified data

--- schema.sql
+++ schema.sql
@@ -10,3 +10,21 @@ CREATE TABLE animals(
 )
 
 ALTER TABLE animals ADD COLUMN species VARCHAR;
+
+CREATE TABLE owners (
+  id SERIAL PRIMARY KEY,
+  full_name VARCHAR(55),
+  age INTEGER
+);
+
+CREATE TABLE species (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(55) NOT NULL
+);
+
+ALTER TABLE animals 
+ADD COLUMN owner_id INTEGER REFERENCES owners(id),
+ADD COLUMN species_id INTEGER REFERENCES species(id),
+DROP COLUMN species
+
+Id is already a primary key and its auto increament. So don't need to add it again.
