cb7a75fc0719848016cc0dedaca33e95f8a85ea0
Rabeccca Nabwire <rabnab12@gmail.com>
2023-04-03 16:53:35 +0300

    Update schema.sql
    queried index on the email inside the owners table

--- schema.sql
+++ schema.sql
@@ -33,5 +33,6 @@ ALTER TABLE owners ADD COLUMN email VARCHAR(120);
 
 CREATE INDEX animal_index ON visits(animal_id);
 CREATE INDEX vet_index ON visits(vet_id);
+CREATE INDEX email_index ON owners(email);
 
 
